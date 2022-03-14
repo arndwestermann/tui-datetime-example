c695e118de2e2b8c8903a57d20248a24dad0a348
Arnd <arnd.westermann@precess.de>
2022-03-14 10:39:10 +0100

    feat(app): added example

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@tui-datetime-test/api-interfaces';
+import { FormGroup, FormControl } from '@angular/forms';
+import { TuiDay, TuiTime, TUI_FIRST_DAY, TUI_LAST_DAY } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'tui-datetime-test-root',
@@ -8,6 +10,37 @@ import { Message } from '@tui-datetime-test/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  options = {
+    is_readonly: false,
+    increment_step: null,
+    max_value: '2022-03-18',
+    min_value: '2022-03-12',
+    precision: null,
+    unit: null,
+  };
+
+  public get minDatetime(): TuiDay | [TuiDay, TuiTime] {
+    if (this.options.min_value) {
+      const date = new Date(this.options.min_value);
+      const day = TuiDay.fromLocalNativeDate(date);
+      const time = TuiTime.fromLocalNativeDate(date);
+      return [day, time];
+    }
+
+    return TUI_FIRST_DAY;
+  }
+
+  public get maxDatetime(): TuiDay | [TuiDay | null, TuiTime | null] {
+    if (this.options.max_value) {
+      const date = new Date(this.options.max_value);
+      const day = TuiDay.fromLocalNativeDate(date);
+      const time = TuiTime.fromLocalNativeDate(date);
+      return [day, time];
+    }
+
+    return TUI_LAST_DAY;
+  }
+  readonly testForm = new FormGroup({
+    testValue: new FormControl([new TuiDay(2017, 2, 15), null]),
+  });
 }
--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { TuiRootModule } from '@taiga-ui/core';
+import { TuiInputDateTimeModule } from '@taiga-ui/kit';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    TuiRootModule,
+    TuiInputDateTimeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
